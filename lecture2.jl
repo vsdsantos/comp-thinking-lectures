1275928576695b416e6d44d8c16059c9a35fe009
Victor Santos <victorsilva223@gmail.com>
2020-11-20 00:39:22 -0300

    change to main

--- lecture2.jl
+++ lecture2.jl
@@ -5,11 +5,19 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 35dbb5f2-2ab1-11eb-3614-e7f46f8b16c2
-using Images
+using Images, Statistics, FFTW, Plots, DSP, ImageFIltering, PlutoUI
+
+# ╔═╡ 2a33b6b0-2ab7-11eb-1987-69485426ad01
+import Pkg; Pkg.add("Statistics")
+
+# ╔═╡ 585724d0-2ab7-11eb-377d-17e95faba7b7
+
 
 # ╔═╡ 54fb60ae-2ab1-11eb-04cf-c15e93f0893d
 
 
 # ╔═╡ Cell order:
 # ╠═35dbb5f2-2ab1-11eb-3614-e7f46f8b16c2
+# ╠═2a33b6b0-2ab7-11eb-1987-69485426ad01
+# ╠═585724d0-2ab7-11eb-377d-17e95faba7b7
 # ╠═54fb60ae-2ab1-11eb-04cf-c15e93f0893d
