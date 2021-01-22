7a1f1c56accd556e4b1e64b7a67c162fb5df4aae
Victor Santos <me@zuckberj.page>
2021-01-21 22:53:47 -0300

    add week 2

---
+++ week2/lecture2.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.12.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 277997be-2b73-11eb-16f7-4f3f7796f140
+using Images
+
+# ╔═╡ b767bd16-2b9f-11eb-2172-4b59677353d7
+dali_path = download("https://www.moma.org/media/W1siZiIsIjM4NjQ3MCJdLFsicCIsImNvbnZlcnQiLCItcXVhbGl0eSA5MCAtcmVzaXplIDIwMDB4MjAwMFx1MDAzZSJdXQ.jpg?sha=fdc1ab6baa7f5400")
+
+# ╔═╡ fae6ff0c-2b9f-11eb-21a8-978b6f791a0f
+dali = load(dali_path)
+
+# ╔═╡ 3425b6f0-2ba0-11eb-0b35-271fc636ae36
+
+
+# ╔═╡ Cell order:
+# ╠═277997be-2b73-11eb-16f7-4f3f7796f140
+# ╠═b767bd16-2b9f-11eb-2172-4b59677353d7
+# ╠═fae6ff0c-2b9f-11eb-21a8-978b6f791a0f
+# ╠═3425b6f0-2ba0-11eb-0b35-271fc636ae36
